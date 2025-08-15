d0756d91d677130d229137ab0e10128a826d1b83
Tolga Yilmaz <85111610+TediTae@users.noreply.github.com>
2025-08-15 12:49:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+from smtplib import SMTP
 
+OWN_EMAIL = "YOUR EMAIL"
+OWN_PASSWORD = "YOUR PASSWORD"
 app = Flask(__name__)
 
 connect = requests.get("https://api.npoint.io/b6b7303ef710d7319459")
@@ -18,10 +21,22 @@ def about():
     header_title = "About"
     return render_template("about.html", title=header_title)
 
-@app.route("/contact")
+@app.get("/contact")
 def contact():
     header_title = "Contact"
-    return render_template("contact.html", title=header_title)
+    return render_template("contact.html", title=header_title, msg_sent=False)
+
+@app.post("/contact")
+def receive_data():
+    header_title = "Contact"
+    name = request.form["name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    message = request.form["message"]
+
+    send_email(name, email, phone, message)
+
+    return render_template("contact.html", title=header_title, msg_sent=True)
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -30,6 +45,13 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
